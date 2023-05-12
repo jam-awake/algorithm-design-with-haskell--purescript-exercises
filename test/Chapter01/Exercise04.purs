ed15f5a0a649279ecc4251adeaf27207e1f6ceca
Jordan Martinez <jordan.martinez@arista.com>
2023-05-12 14:08:32 -0500

    Add additional cases for chp1 ex4

--- test/Chapter01/Exercise04.purs
+++ test/Chapter01/Exercise04.purs
@@ -10,6 +10,10 @@ spec :: Spec Unit
 spec = describe "Exercise 4" do
   it "3 : 2 : 1 : Nil == reverse $ 1 : 2 : 3 : Nil" do
     (3 : 2 : 1 : Nil) `shouldEqual` (reverse $ 1 : 2 : 3 : Nil)
+  it "1 : Nil == reverse $ 1 : 2 : 3 : Nil" do
+    (1 : Nil) `shouldEqual` (reverse $ 1 : Nil)
+  it "Nil == reverse $ Nil" do
+    (Nil :: List Int) `shouldEqual` (reverse $ (Nil :: List Int))
 
 reverse :: forall a. List a -> List a
 reverse = foldl (flip Cons) Nil
