d6683024de757fd064be2cf1a18e236a0021ed42
Jordan Martinez <jordan.martinez@arista.com>
2023-05-17 17:04:11 -0500

    Add chp 3 ex 4

---
+++ test/Chapter03/Exercise04.purs
@@ -0,0 +1,127 @@
+module Test.Chapter03.Exercise04 where
+
+import Prelude
+
+import Data.Foldable (for_)
+import Data.FoldableWithIndex (foldlWithIndex, foldrWithIndex)
+import Data.List (List(..), (:))
+import Data.List as List
+import Data.Maybe (Maybe(..), isNothing)
+import Data.Tuple (Tuple(..))
+import Test.Chapter03.Code.SnocList (SnocList(..), fromListReversed, (<:))
+import Test.Chapter03.Code.SnocList as SnocList
+import Test.Chapter03.Code.SymmetricList (SymmetricList(..))
+import Test.Chapter03.Code.SymmetricList as SymmetricList
+import Test.Spec (Spec, describe, it)
+import Test.Spec.Assertions (shouldEqual)
+
+halveSL :: forall a. SnocList a -> Maybe { head :: a, init :: List a, tail :: SnocList a }
+halveSL ls = do
+  let
+    midPoint = SnocList.length ls / 2
+    value = ls # flip foldrWithIndex Nothing \idx next acc0 -> do
+      if isNothing acc0 then
+        Just $ { head: next, init: Nil, tail: Nil }
+      else if idx <= midPoint then
+        acc0 <#> \acc ->
+          acc { head = next, tail = acc.head : acc.tail }
+      else
+        acc0 <#> \acc ->
+          acc { head = next, init = acc.head : acc.init }
+
+  value <#> \value' -> value' { tail = fromListReversed value'.tail }
+
+unconsSL :: forall a. SymmetricList a -> Maybe { head :: a, tail :: SymmetricList a }
+unconsSL = case _ of
+  Empty ->
+    Nothing
+  Single a ->
+    Just { head: a, tail: Empty }
+  Ends head init tail last ->
+    Just $ { head, tail: _ } $ case init of
+      Cons a init' ->
+        Ends a init' tail last
+      Nil -> case halveSL tail of
+        Nothing -> Single last
+        Just r -> Ends r.head r.init r.tail last
+
+tailSL :: forall a. SymmetricList a -> Maybe (SymmetricList a)
+tailSL = case _ of
+  Empty -> Nothing
+  Single _ -> Just Empty
+  Ends _ init tail last -> Just case List.uncons init of
+    Just initR -> Ends initR.head initR.tail tail last
+    Nothing -> case halveSL tail of
+      Just tailR -> Ends tailR.head tailR.init tailR.tail last
+      Nothing -> Single last
+
+halveList :: forall a. List a -> Maybe { init :: List a, tail :: SnocList a, last :: a }
+halveList ls = do
+  let
+    midPoint = List.length ls / 2
+    value = ls # flip foldlWithIndex Nothing \idx acc0 next -> do
+      if isNothing acc0 then
+        Just { init: SnocNil, tail: SnocNil, last: next }
+      else if idx <= midPoint then
+        acc0 <#> \acc ->
+          acc { init = acc.init <: acc.last, last = next }
+      else
+        acc0 <#> \acc ->
+          acc { tail = acc.tail <: acc.last, last = next }
+
+  value <#> \value' -> value' { init = SnocList.toList value'.init }
+
+unsnocSL :: forall a. SymmetricList a -> Maybe { init :: SymmetricList a, last :: a }
+unsnocSL = case _ of
+  Empty ->
+    Nothing
+  Single a ->
+    Just { init: Empty, last: a }
+  Ends head init tail last -> Just $ { init: _, last } $ case tail of
+    Snoc tail' last' -> Ends head init tail' last'
+    SnocNil -> case halveList init of
+      Nothing -> Single head
+      Just initR -> Ends head initR.init initR.tail initR.last
+
+initSL :: forall a. SymmetricList a -> Maybe (SymmetricList a)
+initSL = case _ of
+  Empty ->
+    Nothing
+  Single _ -> Just Empty
+  Ends head init tail _ -> Just case tail of
+    Snoc tail' last' -> Ends head init tail' last'
+    SnocNil -> case halveList init of
+      Nothing -> Single head
+      Just initR -> Ends head initR.init initR.tail initR.last
+
+spec :: Spec Unit
+spec = describe "Exercise 4" do
+  describe "halveSL" do
+    it "empty" do
+      halveSL (SnocNil :: SnocList Int) `shouldEqual` (Nothing :: Maybe { head :: Int, init :: List Int, tail :: SnocList Int })
+    it "singleton" do
+      halveSL (SnocNil <: 1) `shouldEqual` (Just { head: 1, init: Nil, tail: SnocNil })
+    it "two elems" do
+      halveSL (SnocNil <: 1 <: 2) `shouldEqual` (Just { head: 1, init: Nil, tail: SnocNil <: 2 })
+    it "three elems" do
+      halveSL (SnocNil <: 1 <: 2 <: 3) `shouldEqual` (Just { head: 1, init: Cons 2 Nil, tail: SnocNil <: 3 })
+
+  let
+    inputs =
+      [ Tuple "Empty list" Nil
+      , Tuple "Singleton list" $ pure 1
+      , Tuple "list of 1..10" $ List.range 1 10
+      ]
+  for_ inputs \(Tuple msg i) -> do
+    let symList = SymmetricList.fromList i
+    describe msg do
+      it "unconsSL should work" do
+        let convertTail { head, tail } = { head, tail: SymmetricList.toList tail }
+        (map convertTail $ unconsSL symList) `shouldEqual` List.uncons i
+      it "tailSL should work" do
+        (map SymmetricList.toList $ tailSL symList) `shouldEqual` List.tail i
+      it "unsnocSL should work" do
+        let convertInit { init, last } = { init: SymmetricList.toList init, last }
+        (map convertInit $ unsnocSL symList) `shouldEqual` List.unsnoc i
+      it "initSL should work" do
+        (map SymmetricList.toList $ initSL symList) `shouldEqual` List.init i
--- test/Chapter03/Spec.purs
+++ test/Chapter03/Spec.purs
@@ -5,6 +5,7 @@ import Prelude
 import Test.Chapter03.Exercise01 as Exercise01
 import Test.Chapter03.Exercise02 as Exercise02
 import Test.Chapter03.Exercise03 as Exercise03
+import Test.Chapter03.Exercise04 as Exercise04
 -- import Test.Chapter03.ExerciseX as ExerciseX
 import Test.Spec (Spec, describe)
 
@@ -13,5 +14,6 @@ spec = describe "Chapter 2" do
   Exercise01.spec
   Exercise02.spec
   Exercise03.spec
+  Exercise04.spec
   -- ExerciseX.spec
   pure unit
